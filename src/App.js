4369d121a8a56f28b326988d91956a6321ec45a5
Noi-Git <noi.patsin@gmail.com>
2022-12-13 22:07:58 -0500

    pass delete and create function to its component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import BookCreate from './components/BookCreate'
+import BookList from './components/BookList'
 
 function App() {
+  const [books, setBooks] = useState([])
+
+  const deleteBookById = (id) => {
+    const updatedBook = books.filter((book) => book.id !== id)
+    return setBooks(updatedBook)
+  }
+
+  const createBook = (title) => {
+    const randomId = Math.round(Math.random() * 9999)
+    const updatedBook = [...books, { id: randomId, title }]
+    setBooks(updatedBook)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <BookList books={books} onDelete={deleteBookById} />
+      <BookCreate onCreate={createBook} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
